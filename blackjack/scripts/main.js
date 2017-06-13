77fea1bf72a1817e123abcacb34de99b17748c5f
Jennifer Brown <jendarnellbrown@gmail.com>
2017-06-13 16:41:58 -0400

    blackjack asessment incomplete

---
+++ blackjack/scripts/main.js
@@ -0,0 +1,72 @@
+/* Not sure where to start AT ALL, therefore I tried what "I think I know"
+which looks likes a bunch a messy gibberish code
+
+
+
+
+
+
+
+   Implement a Blackjack hand value calculator.
+
+   Open up the `index.html` file and your console
+   to watch the assertions pass as you write your code.
+
+   Also remember, that the parameter `hand` will be an array, so
+   you'll need to parse through that first before you can start to 
+   write your logic.
+*/
+
+
+function handValue (hand) {
+
+
+  return;
+}
+
+
+/* -----  Hints ------
+
+1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
+K, Q, J ==> Worth 10
+A       ==> Worth 1 or 11
+
+*/
+function handValue(returnTotalValue) {
+  for (let i = 0; i < 22; i++)
+}
+
+
+
+
+
+
+
+function Deck() {
+    this.deck = new Array():
+    this.makeDeck = makeDeck;
+    this.shuffle = shuffle;
+    this.deal = deak;
+
+    function makeDeck() }
+     this.deck = new Array("2", "3", "4", "5", "6", "7", "8", "9", "10"),
+     suits = new Array("A", "K", "Q", "J" ),
+     deck = new Array(52),
+
+     for (i = 0; i < 4; i += 1) {
+        for (j = 0; j < 13; k += 1) {
+            this.deck[i * ranks.length + j] = new Card(ranks[j], suits[i]);
+
+    var random = Math.floor(Math.random() * (this.cards.length - this.cardsDrawn));
+    var temp = this.cards[random];
+
+    for (i = 0; i < n; i += 1) {
+        for (j = 0; j < this.deck.length; j += 1) {
+            k = Math.floor(Math.random() * this.deck.length);
+            temp = this.deck[j];
+            this.deck[j] = this.deck[k];
+            this.deck[k] = temp;
+        }
+    }
+    
+    
